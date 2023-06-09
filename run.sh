60bf2890d9eadf551abbc9a0669d13064b847194
ThameezBo <thameezbo@double.cloud>
2023-06-09 10:22:10 +0200

    fix: use explicit paths

--- run.sh
+++ run.sh
@@ -2,30 +2,26 @@
 
 set -eo pipefail
 
-mkdir -p ~/.kube/
-sed 's?https://0.0.0.0:16443?https://host.docker.internal:16443?g' /tmp/config > ~/.kube/config
+mkdir -p /home/spinnaker/.kube/
+sed 's?https://0.0.0.0:16443?https://host.docker.internal:16443?g' /tmp/config > /home/spinnaker/.kube/config
 
 hal config deploy edit --type localgit --git-origin-user=thameezb
 hal config version edit --version branch:upstream/master
 
 echo "host: 0.0.0.0" | tee \
-    ~/.hal/default/service-settings/gate.yml \
-    ~/.hal/default/service-settings/deck.yml
+    /home/spinnaker/.hal/default/service-settings/gate.yml \
+    /home/spinnaker/.hal/default/service-settings/deck.yml
 
 echo "overrideBaseUrl: redis://redis:6379" | tee \
-    ~/.hal/default/service-settings/redis.yml
+    /home/spinnaker/.hal/default/service-settings/redis.yml
 
 echo "spinnaker.s3.versioning: false" | tee \
-    ~/.hal/default/profiles/front50-local.yml
+    /home/spinnaker/.hal/default/profiles/front50-local.yml
 hal config storage s3 edit --endpoint http://minio:9000 \
     --access-key-id minioadmin \
     --secret-access-key minioadmin \
     --path-style-access true
 hal config storage edit --type s3
-
-hal config provider kubernetes enable
-hal config provider kubernetes account add kind \
-    --context kind-kind
 
 hal config artifact github enable
 hal config artifact github account add spin_local \
@@ -34,5 +30,9 @@ hal config artifact github account add spin_local \
 hal config artifact gitrepo enable
 hal config artifact gitrepo account add spin_local_gitrepo \
     --token-file /home/spinnaker/gh_pat
+
+hal config provider kubernetes enable
+hal config provider kubernetes account add kind \
+    --context kind-kind
 
 hal deploy apply
